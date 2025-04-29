b633d75c892d49be89230c0c984f2522f7ce866c
Maldonado <m.maldonado@dssnetwork.es>
2025-04-29 23:59:22 +0200

    Colocación de componentes astro

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -68,6 +68,15 @@ export default {
           },
         },
       }),
+      animation: {
+        blink: 'blink 1s steps(2, start) infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+      },
     },
   },
   plugins: [
